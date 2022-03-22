1a08d08174ebfb9c2287f5b4d64b6c1067d96578
Vladimir Lebedev <d3fl4t3@gmail.com>
2022-03-23 01:14:23 +0700

    Make binding work

--- wasm-bindings/index.js
+++ wasm-bindings/index.js
@@ -1,8 +1,40 @@
-// Note that a dynamic `import` statement here is required due to
-// webpack/webpack#6615, but in theory `import { greet } from './pkg';`
-// will work here one day as well!
-const rust = import('./pkg');
+import { Client, JsTransport } from './pkg';
 
-rust
-    .then(m => m.greet())
-    .catch(console.error);
+function read_exact(len) {
+    console.log("read " + len + " bytes");
+    return new Uint8Array(len);
+}
+
+function write_all(data) {
+    console.log(data);
+}
+
+function flush() {
+
+}
+
+function _base64ToUint8Array(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes;
+}
+
+let transport = new JsTransport(read_exact, write_all, flush);
+
+let receiver_pub = _base64ToUint8Array("JhXt7H1dZTgxQTIyGiYV4f9VUARuDxFl/1kVBjLSMB8=");
+let sender_pub = _base64ToUint8Array("UDRyFtQ6suh/QUarqr0uAeWapuSbpRdDBKld9yOfAbE=");
+let secret = _base64ToUint8Array("293A2VeFAyl2b9jpo3ChFdIpYlAT6lhJkAwGP53G7Gs=");
+let aes_params = new Uint8Array(160);
+window.crypto.getRandomValues(aes_params);
+
+let client = new Client(receiver_pub, sender_pub, secret, aes_params, transport);
+
+let buffer = _base64ToUint8Array("evmLtDUmPmyV1v7LSX39CqXwMefUEphrXOcgSW21EgUujy0QDN8GjHkENFqtFgAAAAAAAA==");
+let nonce = new Uint8Array(32);
+window.crypto.getRandomValues(nonce);
+client.send(buffer, nonce);
+console.log(client.receive());
--- wasm-bindings/src/lib.rs
+++ wasm-bindings/src/lib.rs
@@ -1,21 +1,15 @@
 #![no_std]
 
+extern crate alloc;
+
 use ciborium_io::{Read, Write};
 use js_sys::{Function, Uint8Array};
 use wasm_bindgen::prelude::*;
-use adnl::{AdnlAddress, AdnlAesParams, AdnlBuilder, AdnlPublicKey, AdnlSecret};
+use adnl::{AdnlAddress, AdnlAesParams, AdnlBuilder, AdnlClient, AdnlPublicKey, AdnlSecret};
+use alloc::vec::Vec;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert(&"Hello!");
-}
-
-#[wasm_bindgen]
+#[derive(Debug)]
 pub struct JsTransport {
     read_exact: Function,
     write_all: Function,
@@ -35,41 +29,70 @@ impl JsTransport {
 }
 
 impl Read for JsTransport {
-    type Error = ();
+    type Error = JsValue;
 
     fn read_exact(&mut self, data: &mut [u8]) -> Result<(), Self::Error> {
-        self.read_exact.call1(&JsValue::null(), &JsValue::from(data.len()));
+        let result = self.read_exact.call1(&JsValue::null(), &JsValue::from(data.len()))?;
+        let result = Uint8Array::from(result);
+        result.copy_to(data);
         Ok(())
     }
 }
 
 impl Write for JsTransport {
-    type Error = ();
+    type Error = JsValue;
 
     fn write_all(&mut self, data: &[u8]) -> Result<(), Self::Error> {
+        self.write_all.call1(&JsValue::null(), &Uint8Array::from(data))?;
         Ok(())
     }
 
     fn flush(&mut self) -> Result<(), Self::Error> {
-        todo!()
+        self.flush.call0(&JsValue::null())?;
+        Ok(())
     }
 }
 
 #[wasm_bindgen]
-pub fn new_client(receiver_addr_js: Uint8Array, sender_pub_js: Uint8Array, secret_js: Uint8Array, aes_params_js: Uint8Array, transport: JsTransport) {
-    let mut aes_params = [0u8; 160];
-    let mut receiver_addr = [0u8; 32];
-    let mut sender_pub = [0u8; 32];
-    let mut secret = [0u8; 32];
-    aes_params_js.copy_to(&mut aes_params);
-    receiver_addr_js.copy_to(&mut receiver_addr);
-    sender_pub_js.copy_to(&mut sender_pub);
-    secret_js.copy_to(&mut secret);
-    let aes_params = AdnlAesParams::from(aes_params);
-    let receiver_addr: AdnlAddress = AdnlPublicKey::from(receiver_addr).into();
-    let sender_pub = AdnlPublicKey::from(sender_pub);
-    let secret = AdnlSecret::from(secret);
-    let client = AdnlBuilder::with_static_aes_params(aes_params)
-        .use_static_ecdh(sender_pub, receiver_addr, secret)
-        .perform_handshake(transport);
+pub struct Client {
+    client: AdnlClient<JsTransport>,
 }
+
+#[wasm_bindgen]
+impl Client {
+    #[wasm_bindgen(constructor)]
+    pub fn new(receiver_pub_js: Uint8Array, sender_pub_js: Uint8Array, secret_js: Uint8Array, aes_params_js: Uint8Array, transport: JsTransport) -> Result<Client, JsValue> {
+        let mut aes_params = [0u8; 160];
+        let mut receiver_pub = [0u8; 32];
+        let mut sender_pub = [0u8; 32];
+        let mut secret = [0u8; 32];
+        aes_params_js.copy_to(&mut aes_params);
+        receiver_pub_js.copy_to(&mut receiver_pub);
+        sender_pub_js.copy_to(&mut sender_pub);
+        secret_js.copy_to(&mut secret);
+        let aes_params = AdnlAesParams::from(aes_params);
+        let receiver_addr: AdnlAddress = AdnlPublicKey::from(receiver_pub).into();
+        let sender_pub = AdnlPublicKey::from(sender_pub);
+        let secret = AdnlSecret::from(secret);
+        let client = AdnlBuilder::with_static_aes_params(aes_params)
+            .use_static_ecdh(sender_pub, receiver_addr, secret)
+            .perform_handshake(transport)
+            .map_err(|e| alloc::format!("{:?}", e))?;
+        Ok(Self { client })
+    }
+
+    #[wasm_bindgen]
+    pub fn send(&mut self, buffer: Uint8Array, nonce: Uint8Array) -> Result<(), JsValue> {
+        let mut nonce: [u8; 32] = nonce.to_vec().try_into().map_err(|e| alloc::format!("{:?}", e))?;
+        self.client.send(&mut buffer.to_vec(), &mut nonce).map_err(|e| alloc::format!("{:?}", e))?;
+        Ok(())
+    }
+
+    #[wasm_bindgen]
+    pub fn receive(&mut self) -> Result<Uint8Array, JsValue> {
+        let mut result = Vec::<u8>::new();
+        self.client.receive::<_, 8192>(&mut result).map_err(|e| alloc::format!("{:?}", e))?;
+        Ok(Uint8Array::from(result.as_slice()))
+    }
+}
+
