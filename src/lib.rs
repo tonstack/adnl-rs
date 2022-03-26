c1ceef5433a5aab7be1c9b87913521e49ca84946
Vladimir Lebedev <d3fl4t3@gmail.com>
2022-03-26 23:46:45 +0700

    Export private key trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub use helper_types::{AdnlAddress, AdnlAesParams, AdnlError, AdnlPublicKey, AdnlSecret, Empty};
+pub use helper_types::{
+    AdnlAddress, AdnlAesParams, AdnlError, AdnlPrivateKey, AdnlPublicKey, AdnlSecret, Empty,
+};
 pub use primitives::handshake::AdnlHandshake;
 pub use primitives::receive::AdnlReceiver;
 pub use primitives::send::AdnlSender;
