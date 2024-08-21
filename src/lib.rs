bad0542b6c652424feba51f2e56bf5e5273bc1ad
Vladimir Lebedev <d3fl4t3@gmail.com>
2024-08-21 22:38:14 +0700

    fix doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # ADNL
 //!
-//! This crate provides a minimal implementation of the Abstract Datagram Network Layer (ADNL) 
+//! This crate provides a minimal implementation of the Abstract Datagram Network Layer (ADNL)
 //! protocol in Rust. ADNL is a network protocol used in The Open Network (TON) blockchain.
 //!
 //! ## Client example
@@ -10,24 +10,24 @@
 //! use base64::Engine as _;
 //! use futures::{SinkExt, StreamExt};
 //! use std::error::Error;
-//! 
+//!
 //! #[tokio::main]
 //! async fn main() -> Result<(), Box<dyn Error>> {
 //!     // decode liteserver public key
 //!     let remote_public = base64::engine::general_purpose::STANDARD.decode("n4VDnSCUuSpjnCyUk9e3QOOd6o0ItSWYbTnW3Wnn8wk=")?;
-//! 
+//!
 //!     // act as a client: connect to ADNL server and perform handshake
 //!     let mut client = AdnlPeer::connect(remote_public, "5.9.10.47:19949").await?;
-//! 
+//!
 //!     // already serialized TL with getTime query
 //!     let query = hex::decode("7af98bb435263e6c95d6fecb497dfd0aa5f031e7d412986b5ce720496db512052e8f2d100cdf068c7904345aad16000000000000")?;
-//! 
+//!
 //!     // send over ADNL
 //!     client.send(query.into()).await?;
-//! 
+//!
 //!     // receive result
 //!     let result = client.next().await.ok_or_else(|| "no result")??;
-//! 
+//!
 //!     // get time from serialized TL answer
 //!     println!(
 //!         "received: {}",
